5058e03d2a78b97981e7c38f55f8c6493a2548e1
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-09 00:36:03 +0000

    task 7

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -17,4 +17,5 @@ BEGIN
 	END IF;
 	INSERT INTO corrections (user_id, project_id, score)`
 	VALUES (user_id, (SELECT id from projects WHERE name=project_name), score);
-END; $$
+END; $
+DELIMITER $$ ;
