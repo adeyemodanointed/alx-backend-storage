b5f2df025a37458a45bd785f7758dce2d1bc96ed
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-12 19:36:35 +0000

    task 15

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,41 @@
 #!/usr/bin/env python3
+"""Log stats - new version"""
+from pymongo import MongoClient
+
+
+def log_stats():
+    """provides some stats about Nginx logs stored in MongoDB"""
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    client = MongoClient('mongodb://localhost:27017')
+    nginx_collection = client.logs.nginx
+
+    count = nginx_collection.count_documents({})
+    print('{} logs'.format(count))
+    print('Methods:')
+    for method in methods:
+        print('{}method {}: {}'.format('\t', method,
+              nginx_collection.count_documents({"method": method})))
+    print('{} status check'.format(
+          nginx_collection.count_documents(
+              {"method": "GET", "path": "/status"})))
+    ips = nginx_collection.aggregate([
+                {
+                    "$group": {
+                        "_id": "$ip",
+                        "count": {"$sum": 1}
+                    }
+                },
+                {
+                    "$sort": {
+                        "count": -1
+                    }
+                },
+                {"$limit": 10}
+        ])
+    print('IPs:')
+    for ip in ips:
+        print('{}{}: {}'.format('\t', ip.get('_id'), ip.get('count')))
+
+
+if __name__ == "__main__":
+    log_stats()
