5d46cb411df67ea932197a08a0a9f8e48e7fdef0
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-12 21:35:23 +0000

    document function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -7,10 +7,12 @@ import uuid
 class Cache():
     """Cache class"""
     def __init__(self):
+        """Initialization"""
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     def store(self, data: bytes) -> str:
+        """Store to redis"""
         id = str(uuid.uuid4())
         self._redis.set(id, data)
         return id
