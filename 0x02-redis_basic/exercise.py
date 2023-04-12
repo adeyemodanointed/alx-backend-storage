bc654fed757e80954afaa9a50bad1b2ba12adfc5
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-12 21:39:31 +0000

    add host to redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,7 @@ class Cache():
     """Cache class"""
     def __init__(self):
         """Initialization"""
-        self._redis = redis.Redis()
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
 
     def store(self, data: bytes) -> str:
