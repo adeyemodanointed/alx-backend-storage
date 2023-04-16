0cb801ceb2520d87c77939033acdb6dbcc958d03
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-16 21:53:22 +0000

    advanced

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import requests
+from typing import Callable
+from functools import wraps
+_redis = __import__('redis').Redis()
+
+
+def count(fn: Callable) -> None:
+    """Counts number of url calls"""
+    @wraps(fn)
+    def wrapper(*args):
+        _redis.incr("count:{}".format(args[0]))
+        return fn(*args)
+    return wrapper
+
+
+@count
+def get_page(url: str) -> str:
+    """Get page on url"""
+    res = _redis.get(url)
+    if (res is not None):
+        return res.decode('utf-8')
+    res = requests.get(url)
+    _redis.set(url, res.text)
+    _redis.expire(url, 10)
+    return res
